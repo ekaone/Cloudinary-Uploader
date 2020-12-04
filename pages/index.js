2b895bc4d199b87c41919aa8cf847011df13ab19
ekaone <ekaone3033@gmail.com>
2020-12-04 15:42:57 +0700

    update index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
+import React, { useMemo, useEffect } from "react";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Uppy from "@uppy/core";
+import { Dashboard } from "@uppy/react";
+import Webcam from "@uppy/webcam";
 
-export default function Home() {
+import styles from "../styles/Home.module.css";
+import "@uppy/core/dist/style.css";
+import "@uppy/dashboard/dist/style.css";
+
+export default function Home(props) {
+  let u = null;
+  if (typeof window !== "undefined") {
+    const uppy = React.useMemo(() => {
+      return Uppy()
+        .use(Webcam)
+        .use(Webcam, { id: "MyWebcam" });
+    }, []);
+    u = uppy;
+  }
+
+  useEffect(() => {
+    return () => uppy.close();
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -9,7 +30,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>Cloudinary</main>
+      <main className={styles.main}>
+        <Dashboard uppy={u} plugins={["Webcam"]} {...props} />
+      </main>
+
+      <footer className={styles.footer}>Copyright</footer>
     </div>
   );
 }
